af35b1dd3eff1cc505c24743c22c2a4998c543b1
Anders Robstad <anders.robstad@live.no>
2025-03-31 11:01:14 +0200

    mocked testing data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to route 1 or 2</h1>
+<!-- <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
+<p>Visit <a href="/route-1">Route 1</a> to read the documentation</p>
+<p>Visit <a href="/route-2">Route 2</a> to read the documentation</p>
---
+++ src/routes/route-1/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  const { data }: PageData = $props();
+
+  const { post, user, timestamp } = $derived(data);
+</script>
+
+<h1>Welcome to Route 1</h1>
+<p>Post: {JSON.stringify(post)}</p>
+<p>User: {JSON.stringify(user)}</p>
+<p>Timestamp: {JSON.stringify(timestamp)}</p>
