4506b748df70ce9287d8e2aea87229934dfb4d53
Anders Robstad <anders.robstad@live.no>
2025-03-31 12:01:30 +0200

    ssr and remove route from main

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,3 @@
-<h1>Welcome to route 1 or 2</h1>
-<!-- <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
-<p>Visit <a href="/route-1">Route 1</a> to read the documentation</p>
-<p>Visit <a href="/route-2">Route 2</a> to read the documentation</p>
+<h1>Loosing internet connection routing bug</h1>
+
+<a href="/route-1">Visit Route 1</a>
