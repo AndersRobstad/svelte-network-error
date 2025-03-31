980a01de395c60e37d402744dec51d3e1f3e2a1b
Anders Robstad <anders.robstad@live.no>
2025-03-31 11:58:57 +0200

    remove extra route and enable ssr

--- src/routes/route-1/+page.svelte
+++ src/routes/route-1/+page.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
   import type { PageData } from "./$types";
 
-  const { data }: PageData = $props();
-
-  const { post, user, timestamp } = $derived(data);
 </script>
 
 <h1>Welcome to Route 1</h1>
-<p>Post: {JSON.stringify(post)}</p>
-<p>User: {JSON.stringify(user)}</p>
-<p>Timestamp: {JSON.stringify(timestamp)}</p>
+<a href="/">Go back</a>
